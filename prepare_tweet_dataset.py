cb5a7a1802af1da15a0f8098ba6b851173774888
AtomAnu <a.anuchitanukul@gmail.com>
2021-10-21 08:53:39 +0100

    Updated prepare_tweet_dataset.py

--- prepare_tweet_dataset.py
+++ prepare_tweet_dataset.py
@@ -6,12 +6,12 @@ from ekphrasis.dicts.emoticons import emoticons
 """
 Data:
     - hateful_founta_filt_nontox/train.jsonl -> train.pos dev.pos
-    - hateful_founta_filt_toxic_full/train_nort.jsonl -> train.neg dev.neg
+    - hateful_founta_toxic_full/train_nort.jsonl -> train.neg dev.neg
 
 """
 
 nontox_train_path = 'data/tweet/hateful_founta_filt_nontox/train.jsonl'
-tox_train_path = 'data/tweet/hateful_founta_filt_toxic_full/train_nort.jsonl'
+tox_train_path = 'data/tweet/hateful_founta_toxic_full/train_nort.jsonl'
 val_path = 'data/tweet/val.jsonl'
 
 nontox_train_text_list = jsonl_reader(nontox_train_path)
