2acd8115f6787501d8faf2a2bd01879ddc8e4b77
AtomAnu <a.anuchitanukul@gmail.com>
2021-10-21 08:49:08 +0100

    Updated prepare_tweet_dataset.py

--- prepare_tweet_dataset.py
+++ prepare_tweet_dataset.py
@@ -6,13 +6,12 @@ from ekphrasis.dicts.emoticons import emoticons
 """
 Data:
     - hateful_founta_filt_nontox/train.jsonl -> train.pos dev.pos
-    - hateful_founta_filt_tox/train.jsonl -> train.neg dev.neg
-    - val.jsonl -> test.pos test.neg
+    - hateful_founta_filt_toxic_full/train_nort.jsonl -> train.neg dev.neg
 
 """
 
 nontox_train_path = 'data/tweet/hateful_founta_filt_nontox/train.jsonl'
-tox_train_path = 'data/tweet/hateful_founta_filt_tox/train.jsonl'
+tox_train_path = 'data/tweet/hateful_founta_filt_toxic/train_nort.jsonl'
 val_path = 'data/tweet/val.jsonl'
 
 nontox_train_text_list = jsonl_reader(nontox_train_path)
@@ -29,6 +28,8 @@ dev_pos_path = 'data/tweet/dev.pos'
 dev_neg_path = 'data/tweet/dev.neg'
 test_pos_path = 'data/tweet/test.pos'
 test_neg_path = 'data/tweet/test.neg'
+
+
 
 text_processor = TextPreProcessor(
     # terms that will be omitted
@@ -56,5 +57,6 @@ write_file(train_pos_path, nontox_train_text_list, text_processor)
 write_file(dev_pos_path, nontox_train_text_list, text_processor)
 write_file(train_neg_path, tox_train_text_list, text_processor)
 write_file(dev_neg_path, tox_train_text_list, text_processor)
-write_file(test_pos_path, val_text_list, text_processor)
-write_file(test_neg_path, val_text_list, text_processor)
+# write_file(test_pos_path, val_text_list, text_processor)
+# write_file(test_neg_path, val_text_list, text_processor)
+
